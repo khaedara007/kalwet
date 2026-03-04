b8b8061d3a09bd21d0c51bc6b2a75ea7ecac662f
khaedara007 <faiqkhaedara@gmail.com>
2026-03-05 04:48:34 +0700

    coba10

--- application/controllers/AdminAuth.php
+++ application/controllers/AdminAuth.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class AdminAuth extends CI_Controller
+class Adminauth extends CI_Controller
 {
     public function __construct()
     {
